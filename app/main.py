510bdbe81ab86609453fe1a72ca99f48c1f43e74
scptx0 <134897241+scptx0@users.noreply.github.com>
2025-11-24 01:40:09 -0500

    Implementar lectura de variables de entorno en app
    Además, se reconstruyó la imagen y se subió la actualización a docker-hub

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -8,7 +9,16 @@ def health():
 
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify({"message": "Hola mundo"}), 200
+    # Obtener variables de entorno
+    environment = os.getenv("ENV", "local")
+    api_key = os.getenv("API_KEY", "no-key-found")
+
+    return jsonify({
+        "message": "Hola mundo",
+        "environment": environment,
+        "api_key_status": "found" if api_key != "no-key-found" else "missing"
+    }), 200
 
 if __name__ == "__main__":
+    # Escucha en todas las interfaces para exponer en contenedor
     app.run(host="0.0.0.0", port=5000)
